a00a0ea27eb872f648d3171446ac4250f47b3412
jgeller11 <34071775+jgeller11@users.noreply.github.com>
2017-12-07 15:33:05 -0500

    Added comments

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from cipher import cipher
 from saver import saver
 from translate import Numcode
 from morse import morse
-
+# import all the classes
 print('What type of message would you like to send?')
 print('Morse Code')
 print('Cipher')
 print('Numeric')
 print('')
-messageType=input().lower()
-while messageType!='cipher' and messageType!='morse code' and messageType!='numeric':
+messageType=input().lower() #which translation should be used
+while messageType!='cipher' and messageType!='morse code' and messageType!='numeric': #go until it works
     print('Sorry, I didn\'t get that, try again?')
     messageType=input().lower()
-print('Please type your message here:')
+print('Please type your message here:') #get information about sender, recipient, and message
 message=input()
 print('For whom is this message?')
 location=input()
@@ -24,4 +24,4 @@ if messageType=='numeric':
           export=Numcode(message)
 if messageType=='morse code':
           export=morse(message)
-finish=saver(export.translation, sender, location)
+finish=saver(export.translation, sender, location) #save file using all the given information
