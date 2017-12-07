9118efa56e4ca8f276df3116483fbd097fd885fc
jgeller11 <34071775+jgeller11@users.noreply.github.com>
2017-12-07 15:09:47 -0500

    Create morse.py

---
+++ morse.py
@@ -0,0 +1,11 @@
+class morse:
+	def __init__(self, message):
+		self.message = message.lower()
+		self.translation = ''
+		self.alpha = 'abcdefghijklmnopqrstuvwxyz'
+		self.code = [ '.- / ', '-... / ', '-.-. / ', '-.. / ', '. / ', '..-. / ', '--. / ', '.... / ', '.. / ', '.--- / ', '-.- / ', '.-.. / ', '-- / ', '-. / ', '--- / ', '.--. / ', '--.- / ', '.-. / ', '... / ', '- / ', '..- / ', '...- / ', '.-- / ', '-..- / ', '-.-- / ', '--.. / ']
+		for x in range(len(self.message)):
+			if self.message[x].isalpha():
+				self.translation = self.translation + self.code[self.alpha.find(self.message[x].lower())]
+			else:
+				self.translation = self.translation + self.code[x]
